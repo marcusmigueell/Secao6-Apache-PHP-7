a35cbbada98010f3cd081acb035aed50fb6bf035
Marcus Miguel <marcusmigueell@gmail.com>
2021-06-08 19:29:28 -0300

    Creating The Default Master

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+
+    <?php
+
+        echo 'Utilizando a tag padrão';
+
+    ?>
+
+    <br>
+
+    <?= 'Utilizando a tag impressão' ?>
+
+    <br>
+
+    <? echo 'Utilizando a tag curta'; ?>
+    
+</body>
+
+</html>
