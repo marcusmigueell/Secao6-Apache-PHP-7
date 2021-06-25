d70c690029c6a335490fe65266a4b0a2733aa970
Marcus Miguel <marcusmigueell@gmail.com>
2021-06-25 18:28:12 -0300

    User Profile Control Rule

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -76,6 +76,14 @@
                 <?php
 
                   $chamado_dados = explode('#', $chamado);
+
+                  if($_SESSION['perfil_id'] == 2) {
+                    //só vamos exibir o chamado, se ele foi criado pelo usuário
+                    if($_SESSION['id'] != $chamado_dados[0]){
+                      continue;
+                    }
+                  }
+
                   if(count($chamado_dados) < 3) {
                     continue;
                   }
@@ -86,9 +94,9 @@
 
                   <div class="card-body">
 
-                    <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
-                    <p class="card-text"><?= $chamado_dados[2] ?></p>
+                    <h5 class="card-title"><?= $chamado_dados[1] ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[2] ?></h6>
+                    <p class="card-text"><?= $chamado_dados[3] ?></p>
 
                   </div>
 
